fdd3e0b4fc013fa4d2bd60b7b1862b55b691e038
Nikos Norman <nikos_norman87@hotmail.com>
2024-12-09 11:29:16 +0000

    Add python lists

---
+++ lists.py
@@ -0,0 +1,13 @@
+import os
+
+#clear terminal
+os.system("clear")
+
+#create empty list
+my_list = [1, 2, 3, "bananas", "apples", [4,5,6] ]
+
+#access list elements
+
+my_list.append("oranges")
+
+print(my_list[3])
